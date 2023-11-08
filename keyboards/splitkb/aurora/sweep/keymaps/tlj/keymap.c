90187652200039acd662e94fd5c5d9c338ca0ea3
tlj <tlj@tlj.no>
2023-11-08 12:06:29 +0100

    Sweep - Formatted kb layout, re-added RGB matrix
    Found the problem with the RGB on the right hand keyboard thanks to Yingeling on discord

--- keyboards/splitkb/aurora/sweep/keymaps/tlj/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/tlj/keymap.c
@@ -1,11 +1,36 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, LALT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), LT(3,KC_G), KC_H, RSFT_T(KC_J), RGUI_T(KC_K), RCTL_T(KC_L), RALT_T(KC_SCLN), KC_Z, KC_X, KC_C, KC_V, KC_B, LT(2,KC_N), KC_M, KC_COMM, KC_DOT, KC_ENT, LT(1,KC_SPC), MEH_T(KC_TAB), LT(4,KC_BSPC), LT(1,KC_SPC)),
-	[1] = LAYOUT(KC_NO, KC_PERC, KC_LCBR, KC_RCBR, KC_CIRC, KC_NO, KC_GRV, KC_QUOT, KC_NO, KC_NO, KC_LSFT, KC_AT, KC_LPRN, KC_RPRN, KC_DLR, KC_SLSH, KC_COLN, KC_EQL, KC_DQUO, KC_RSFT, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_ASTR, KC_NO, KC_BSLS, KC_MINS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-	[2] = LAYOUT(KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LSFT, KC_NO, KC_4, KC_5, KC_6, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_LSFT, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_0, KC_PENT, KC_NO, KC_NO),
-	[3] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-	[4] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, BL_STEP, BL_DOWN, BL_UP, BL_TOGG, KC_NO, KC_NO, KC_NO, KC_ESC, KC_ENT, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPLY, KC_MPRV, KC_MNXT, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, KC_NO)
+	[0] = LAYOUT(
+        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,            KC_Y,          KC_U,         KC_I,         KC_O,         KC_P,
+        LALT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), LT(3,KC_G),      KC_H,          RSFT_T(KC_J), RGUI_T(KC_K), RCTL_T(KC_L), RALT_T(KC_SCLN),
+        KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,            LT(2,KC_N),    KC_M,         KC_COMM,      KC_DOT,       KC_ENT,
+                                                  LT(1,KC_SPC), MEH_T(KC_TAB),   LT(4,KC_BSPC), LT(1,KC_SPC)
+    ),
+	[1] = LAYOUT(
+        KC_NO,        KC_PERC,      KC_LCBR,      KC_RCBR,      KC_CIRC,         KC_NO,         KC_GRV,       KC_QUOT,      KC_NO,        KC_NO,
+        KC_LSFT,      KC_AT,        KC_LPRN,      KC_RPRN,      KC_DLR,          KC_SLSH,       KC_COLN,      KC_EQL,       KC_DQUO,      KC_RSFT,
+        KC_NO,        KC_NO,        KC_LBRC,      KC_RBRC,      KC_ASTR,         KC_NO,         KC_BSLS,      KC_MINS,      KC_NO,        KC_NO,
+                                                  KC_NO,        KC_NO,           KC_NO,         KC_NO
+    ),
+	[2] = LAYOUT(
+        KC_NO,        KC_NO,        KC_7,         KC_8,         KC_9,            KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_NO,
+        KC_LSFT,      KC_NO,        KC_4,         KC_5,         KC_6,            KC_NO,         KC_LSFT,      KC_NO,        KC_NO,        KC_LSFT,
+        KC_NO,        KC_NO,        KC_1,         KC_2,         KC_3,            KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_NO,
+                                                  KC_0,         KC_PENT,         KC_NO,         KC_NO
+    ),
+	[3] = LAYOUT(
+        KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,           KC_F6,         KC_F7,        KC_F8,        KC_F9,        KC_F10,
+        KC_LALT,      KC_LCTL,      KC_LGUI,      KC_LSFT,      KC_NO,           KC_LEFT,       KC_DOWN,      KC_UP,        KC_RGHT,      KC_NO,
+        KC_F11,       KC_F12,       KC_NO,        KC_NO,        KC_NO,           KC_HOME,       KC_PGDN,      KC_PGUP,      KC_END,       KC_NO,
+                                                  KC_NO,        KC_NO,           KC_NO,         KC_NO
+    ),
+	[4] = LAYOUT(
+        RGB_MOD,      RGB_HUI,      RGB_SAI,      RGB_VAI,      RGB_SPI,         KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_NO,
+        RGB_TOG,      KC_NO,        KC_ESC,       KC_ENT,       KC_NO,           KC_MUTE,       KC_VOLD,      KC_VOLU,      KC_NO,        KC_NO,
+        RGB_RMOD,     RGB_HUD,      RGB_SAD,      RGB_VAD,      RGB_SPD,         KC_MPLY,       KC_MPRV,      KC_MNXT,      KC_NO,        KC_NO,
+                                                  KC_NO,        QK_BOOT,         KC_NO,         KC_NO
+    )
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
