1f8a887ceca8586d1d58455dd6c1b426c376a61f
tlj <tlj@tlj.no>
2023-11-11 10:50:30 +0100

    Sweep - added amp and pipe to symbol layer, added unicode layer

--- keyboards/splitkb/aurora/sweep/keymaps/tlj/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/tlj/keymap.c
@@ -10,6 +10,25 @@ enum layers {
     _NAVI,
     _MEDIA,
     _MOUSE,
+    _NORDIC,
+};
+
+enum unicode_names {
+    AE_LOWER,
+    AE_UPPER,
+    OE_LOWER,
+    OE_UPPER,
+    AA_LOWER,
+    AA_UPPER,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [AE_LOWER] = 0x00E6,
+    [AE_UPPER] = 0x00C6,
+    [OE_LOWER] = 0x0153,
+    [OE_UPPER] = 0x0152,
+    [AA_LOWER] = 0x00E5,
+    [AA_UPPER] = 0x00C5,
 };
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -20,17 +39,38 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return rotation;
 }
 
+#define KC_ALTA LALT_T(KC_A)
+#define KC_CTRS LCTL_T(KC_S)
+#define KC_GUID LGUI_T(KC_D)
+#define KC_SHFF LSFT_T(KC_F)
+#define KC_GNAV LT(_NAVI,KC_G)
+#define KC_HNAV LT(_NUMBERS,KC_H)
+#define KC_SHFJ RSFT_T(KC_J)
+#define KC_GUIK RGUI_T(KC_K)
+#define KC_CTRL RCTL_T(KC_L)
+#define KC_ALTS RALT_T(KC_SCLN)
+#define KC_NORC LT(_NORDIC, KC_C)
+#define KC_SYMC LT(_SYMBOLS, KC_SPC)
+#define KC_MEHT MEH_T(KC_TAB)
+#define KC_SYBS LT(_SYMBOLS, KC_BSPC)
+#define KC_GUIR LGUI(KC_R)
+#define KC_GUIL LGUI(KC_LBRC)
+#define KC_GURB LGUI(KC_RBRC)
+#define KC_NOAE UP(AE_LOWER, AE_UPPER)
+#define KC_NOOE UP(OE_LOWER, OE_UPPER)
+#define KC_NOAA UP(AA_LOWER, AA_UPPER)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DEFAULT] = LAYOUT(
         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,            KC_Y,          KC_U,         KC_I,         KC_O,         KC_P,
-        LALT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), LT(3,KC_G),      LT(2,KC_H),    RSFT_T(KC_J), RGUI_T(KC_K), RCTL_T(KC_L), RALT_T(KC_SCLN),
-        KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,            KC_N,          KC_M,         KC_COMM,      KC_DOT,       KC_ENT,
-                                                  LT(1,KC_SPC), MEH_T(KC_TAB),   LT(4,KC_BSPC), LT(1,KC_SPC)
+        KC_ALTA,      KC_CTRS,      KC_GUID,      KC_SHFF,      KC_GNAV,         KC_HNAV,       KC_SHFJ,      KC_GUIK,      KC_CTRL,      KC_ALTS,
+        KC_Z,         KC_X,         KC_NORC,      KC_V,         KC_B,            KC_N,          KC_M,         KC_COMM,      KC_DOT,       KC_ENT,
+                                                  KC_SYMC,      KC_MEHT,         KC_SYBS,       KC_SYMC
     ),
 	[_SYMBOLS] = LAYOUT(
-        KC_NO,        KC_PERC,      KC_LCBR,      KC_RCBR,      KC_CIRC,         KC_NO,         KC_GRV,       KC_QUOT,      KC_NO,        KC_NO,
+        KC_NO,        KC_PERC,      KC_LCBR,      KC_RCBR,      KC_CIRC,         KC_AMPR,       KC_GRV,       KC_QUOT,      KC_NO,        KC_NO,
         KC_LSFT,      KC_AT,        KC_LPRN,      KC_RPRN,      KC_DLR,          KC_SLSH,       KC_COLN,      KC_EQL,       KC_DQUO,      KC_RSFT,
-        KC_NO,        KC_NO,        KC_LBRC,      KC_RBRC,      KC_ASTR,         KC_NO,         KC_BSLS,      KC_MINS,      KC_NO,        KC_NO,
+        KC_NO,        KC_EXCLAIM,   KC_LBRC,      KC_RBRC,      KC_ASTR,         KC_PIPE,       KC_BSLS,      KC_MINS,      KC_NO,        KC_NO,
                                                   KC_NO,        KC_NO,           KC_NO,         KC_NO
     ),
 	[_NUMBERS] = LAYOUT(
@@ -54,9 +94,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MOUSE] = LAYOUT(
         KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,           KC_NO,         KC_WH_D,      KC_WH_U,      KC_NO,        KC_NO,
         KC_NO,        KC_NO,        KC_BTN2,      KC_BTN1,      KC_NO,           KC_MS_L,       KC_MS_D,      KC_MS_U,      KC_MS_R,      KC_NO,
-        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,           LGUI(KC_R),    LGUI(KC_LBRC),LGUI(KC_RBRC),KC_NO,        TG(_MOUSE),
+        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,           KC_GUIR,       KC_GUIL,      KC_GURB,      KC_NO,        TG(_MOUSE),
                                                   KC_NO,        KC_NO,           KC_BTN2,       KC_BTN1
-    )
+    ),
+	[_NORDIC] = LAYOUT(
+        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,           KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_NO,
+        KC_NO,        KC_NO,        KC_NO,        KC_LSFT,      KC_NO,           KC_NO,         KC_NOAE,      KC_NOOE,      KC_NOAA,      KC_NO,
+        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,           KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_NO,
+                                                  KC_NO,        KC_NO,           KC_NO,         KC_NO
+    ),
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
